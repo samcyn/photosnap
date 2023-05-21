d94e4906ed6db1ec24bcf4deda0588cf4f4e5732
samcyn <samsoniyanda@outlook.com>
2023-05-21 23:50:46 +0100

    fix: fixed context issue with gatsby config

---
+++ gatsby-ssr.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import GetInviteProvider from './src/Providers/GetInviteContext';
+import './src/assets/styles/global.css';
+
+export const wrapRootElement = ({ element }) => (
+  <GetInviteProvider>{element}</GetInviteProvider>
+)
